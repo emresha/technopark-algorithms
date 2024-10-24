66ec9e183676ee26cb331252e677c358245921a9
Emre Demirel <emredemirel2020@yandex.ru>
2024-10-24 14:23:43 +0300

    task3 seems to be done

---
+++ hw1/task3.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+
+class Queue 
+{
+    public:
+        int* arr = NULL;
+        int front = 0;
+        int rear = 0;
+        int size = -1;
+        int capacity = 0;
+
+        Queue(int size);
+        ~Queue();
+
+        int popFront();
+        void pushBack(int n);
+        bool isFull();
+        bool isEmpty();
+};
+
+Queue::Queue(int size) 
+{
+    capacity = size;
+    arr = new int[capacity];
+    front = 0;
+    rear = 0;
+    this->size = 0;
+}
+
+Queue::~Queue() 
+{
+    delete[] arr;
+}
+
+bool Queue::isFull() 
+{
+    return size == capacity;
+}
+
+bool Queue::isEmpty() 
+{
+    return size == 0;
+}
+
+int Queue::popFront() 
+{
+    if (isEmpty()) 
+        return -1;
+
+    int element = arr[front];
+    front = (front + 1) % capacity;
+    size--;
+    return element;
+}
+
+void Queue::pushBack(int n) 
+{
+    if (isFull()) 
+    {
+        // std::cout << "realloc\n" << std::endl;
+        int* newArr = new int[capacity * 2];
+        
+        for (int i = 0; i < size; i++) 
+        {
+            newArr[i] = arr[(front + i) % capacity];
+        }
+        
+        delete[] arr;
+        arr = newArr;
+        front = 0;
+        rear = size;
+        capacity *= 2;
+    }
+
+    arr[rear] = n;
+    rear = (rear + 1) % capacity;
+    size++;
+}
+
+int main() 
+{
+    int initial_size = 4;
+    Queue q(initial_size);
+
+    int n;
+
+    scanf("%d", &n);
+
+    // переменная для ответа
+    std::string answer = "YES";
+
+    // чтение входных данных
+    for (int i = 0; i < n; i++)
+    {
+        int action, num;
+        action = num = 0;
+        scanf("%d %d", &action, &num);
+        if (action == 2)
+        {
+            if (q.popFront() != num)
+                answer = "NO";
+        }
+        else if (action == 3)
+            q.pushBack(num);
+    
+    }
+
+    std::cout << answer << std::endl;
+
+    return 0;
+}
